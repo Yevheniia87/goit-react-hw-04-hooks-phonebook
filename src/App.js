7c071deae87d6f3b95507957dc08bafe9171c47a
Євгенія <76601332+Yevheniia87@users.noreply.github.com>
2021-09-03 14:49:15 +0300

    added files

--- src/App.js
+++ src/App.js
@@ -8,14 +8,19 @@ import defaultContacts from "./component/data/defaultContacts.json";
 //import { Component } from "react";
 // const useLocalStorage = (contacts, defaultContacts) => {
 //   const [state, setState] = useState(() => {
-//     return JSON.parse(window.localStorage.getItem(contacts)) ?? defaultContacts;
+//     return JSON.parse(window.localStorage.getItem('contacts')) ?? defaultContacts;
 //   });
 //   useEffect(() => {
 //     window.localStorage.setItem(contacts, JSON.stringify(state));
 //   }, [contacts, state]);
 //   return [state, setState];
 // };
-
+// const defaultContact = [
+//   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+//   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+//   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+//   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+// ];
 export default function App() {
   const [contacts, setContacts] = useState(() => {
     return (
@@ -44,13 +49,14 @@ export default function App() {
     };
     setContacts((contacts) => [newContact, ...contacts]);
   };
-  const changeFilter = (e) => {
-    setFilter(e.currentTarget.value);
-  };
+
   const checkContact = () => {
     return contacts.filter((contact) =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
+  };
+  const changeFilter = (e) => {
+    setFilter(e.currentTarget.value);
   };
 
   const removeContact = (contactId) => {
